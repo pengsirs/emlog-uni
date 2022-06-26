691177fea970eb2647896ee8e3c0e547de525444
pengsirs <hk@hkiii.cn>
2022-06-26 13:12:10 +0800

    程序设置

---
+++ setting.js
@@ -0,0 +1,17 @@
+const url = 'https://www.hkiii.cn';// 域名设置
+const $apikey = '21afd3a75a3f465cec89a2e57997aa4f';//API密钥
+const tenapi = 'https://api.tianapi.com';//请勿修改，调用百度收录等接口
+const tiankey = 'dedce66fbabebe6b94677ac20fbfa785';//天行数据key，获取地址www.tianapi.com
+// 七牛云配置
+const $accessKey = 'onk7a3DkIlOdm0ct8ga5M9iUr007mLOGRbyV7qcL';//七牛云ak
+const $secretKey = 'dnLtxGA1nFcx1Pd5kB6UA1eKGXCGD4o9g1WvVldp';//七牛云sk
+const $bucket = 'hkiii';//七牛云空间名
+export default {  
+    url,
+	$apikey,
+	tenapi,
+	tiankey,
+	$accessKey,
+	$secretKey,
+	$bucket
+}
