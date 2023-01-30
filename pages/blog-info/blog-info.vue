1c28216e4cfc261a716eb3cca9755bcd78beb734
大彭Sir <hk@hkiii.cn>
2023-01-30 23:21:17 +0800

    ce

--- pages/blog-info/blog-info.vue
+++ pages/blog-info/blog-info.vue
@@ -97,7 +97,7 @@
 							<view style="font-size: 12px;font-weight: 400;">问题反馈</view>
 						</view>
 					</view>
-					<view class="homelist" v-if="appData.auditing != 1" @click="down()">
+					<view class="homelist" v-if="appData.data.auditing != 1" @click="down()">
 						<view class="homelist-item">
 							<uni-icons type="download-filled" color="#fec855" size="30"></uni-icons>
 							<view style="font-size: 12px;font-weight: 400;">附件下载</view>
